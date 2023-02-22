eb5906d8295664a14e5adc3e706c5b38e546456b
IllaBulich <mba.2004@mail.ru>
2023-02-22 21:33:54 +0300

    Работа с базой данных MySQL. Создание модели

--- src/main/java/com/example/web/controllers/ImmovablesController.java
+++ src/main/java/com/example/web/controllers/ImmovablesController.java
@@ -1,5 +1,8 @@
 package com.example.web.controllers;
 
+import com.example.web.models.Post;
+import com.example.web.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ImmovablesController {
 
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/immovables")
     public String immovablesMain(Model  module){
+
+        Iterable<Post> posts = postRepository.findAll();
+        module.addAttribute("posts",posts);
         return "immovables-main";
     }
 
