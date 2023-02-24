fc994a39250facf124154f12a9318342753762e1
IllaBulich <mba.2004@mail.ru>
2023-02-24 23:19:32 +0300

    Форма добавления записей через сайт

--- src/main/java/com/example/web/controllers/ImmovablesController.java
+++ src/main/java/com/example/web/controllers/ImmovablesController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ImmovablesController {
@@ -19,6 +21,21 @@ public class ImmovablesController {
         Iterable<Post> posts = postRepository.findAll();
         module.addAttribute("posts",posts);
         return "immovables-main";
+    }
+    @GetMapping("/immovables/add")
+    public String immovablesAdd(Model  module){
+        return "immovables-add";
+    }
+
+    @PostMapping("/immovables/add")
+    public String immovablesPostAdd(
+            @RequestParam String title,
+            @RequestParam String anons,
+            @RequestParam String full_text,
+            Model model){
+        Post post = new Post(title,anons,full_text);
+        postRepository.save(post);
+        return "redirect:/immovables";
     }
 
 }
