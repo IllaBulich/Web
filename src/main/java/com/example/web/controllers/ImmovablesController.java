12b8d8a3486cd0ca0fce79d398341c27eced5b06
IllaBulich <mba.2004@mail.ru>
2023-02-24 23:47:40 +0300

    Вывод каждой из статей

--- src/main/java/com/example/web/controllers/ImmovablesController.java
+++ src/main/java/com/example/web/controllers/ImmovablesController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class ImmovablesController {
@@ -36,6 +40,21 @@ public class ImmovablesController {
         Post post = new Post(title,anons,full_text);
         postRepository.save(post);
         return "redirect:/immovables";
+    }
+
+    @GetMapping("/immovables/{id}")
+    public String immovablesDetails(
+            @PathVariable(value = "id") long id,
+            Model  model){
+
+        if (!postRepository.existsById(id))
+            return "redirect:/immovables";
+
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res =new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post",res);
+        return "immovables-details";
     }
 
 }
